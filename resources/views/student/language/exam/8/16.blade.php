4f7ed7fec82970c96676c9f8128b6fb90c962945
Thanakit.P <78790513+apichart52150@users.noreply.github.com>
2021-08-23 11:31:45 +0700

    submit

--- resources/views/student/language/exam/8/16.blade.php
+++ resources/views/student/language/exam/8/16.blade.php
@@ -1,1 +1,277 @@
-<p>None</p>
+<style>
+    .form-control {
+        border: none;
+        border-bottom: 1px solid #ccc;
+        display: inline-block;
+        height: auto;
+        width: 135px;
+        padding: 0 5px;
+        margin: 10px 5px;
+    }
+
+    ul.questions li {
+        margin: 20px 0;
+        line-height: 2rem;
+    }
+
+    .input-con {
+        display: inline-block;
+        position: relative;
+    }
+
+    .ans-con {
+        position: absolute;
+        top: 50%;
+        right: 5px;
+        transform: translateY(-50%);
+    }
+</style>
+@php
+    //$question -> input
+    $q1 = "The flow-chart shows the";
+    $q2 = "involved in diamond processing, from extracting the ore to producing the diamond concentrate. The process consists of three";
+    $q3 = "stages: crushing, cleaning and separation. Each stage is";
+    $q4 = "out using a number of different machines. The first stage in the process is crushing, which itself";
+    $q5 = "of three main stages. First of all, the iron ore is passed to the primary crusher,";
+    $q6 = "the large rocks are crushed into smaller pieces of roughly 30 centimetres in diameter. These smaller pieces are then fed into the secondary crusher, which, in";
+    $q7 = ", reduces the rocks to an even smaller size: 8 cm in diameter. The";
+    $q8 = "stage in crushing involves the roller crusher, which breaks up the rocks into pieces not";
+    $q9 = "3 centimetres, at the same time releasing the diamonds. During the next stage, cleaning, four machines are used to";
+    $q10 = "various pieces of dirt from the diamond ore. The primary and the secondary scrubber remove dirt particles and reduce the size of the ore further. As their names imply, the de-gritter removes grit and the de-sander gets rid of";
+    $q11 = ". The separation stage consists of three methods. In the first of these, heavy minerals, which surround the diamonds, are separated from the light minerals, and the";
+    $q12 = "are discarded.";
+    $end = "this, magnets remove unwanted magnetic deposits, and finally the pure diamond ore is extracted using a grease table which separates the diamond concentrate from all the remaining pieces.";
+@endphp
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-box text-dark font-15">
+            <div class="row justify-content-center mb-2">
+                <div class="col-md-12">
+                    <div class="border border-dark px-2 text-center">
+                        <h5>The diagram shows the process of diamond mining, from the raw material (ore) to the diamond concentrate. Write a description of the process showing how the ore is transformed into diamond concentrate.</h5>
+                        <h5>Summarize the information by selecting and reporting on the main features, and make comparisons where relevant. </h5>
+                        <img src="{{ asset('public/img_lang/gap2/gap2_16.jpg') }}" class="img-fluid mb-2" alt="Responsive image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-box text-dark font-15">
+            <div class="row justify-content-center mb-2">
+                <div class="col-md-12">
+                    <div class="border border-dark p-2">
+                            {{$q1}}
+                            <span class="font-weight-bold">1.</span> 
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q2}}
+                            <span class="font-weight-bold">2.</span> 
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q3}}
+                            <span class="font-weight-bold">3.</span> 
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q4}}
+                            <span class="font-weight-bold">4.</span> 
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q5}}
+                            <span class="font-weight-bold">5.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q6}}
+                            <span class="font-weight-bold">6.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q7}}
+                            <span class="font-weight-bold">7.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q8}}
+                            <span class="font-weight-bold">8.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q9}}
+                            <span class="font-weight-bold">9. </span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q10}}
+                            <span class="font-weight-bold">10.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q11}}
+                            <span class="font-weight-bold">11.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$q12}}
+                            <span class="font-weight-bold">12.</span>
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                            {{$end}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@section('button-control')
+    <button id="check-answer" class="btn btn-info">Check Answers</button>
+@endsection
+
+@section('js')
+<script>
+    // no space answer
+    const answer1 = ['stages'];
+    const answer2 = ['distinct', 'different'];
+    const answer3 = ['carried'];
+    const answer4 = ['consists'];
+    const answer5 = ['where'];
+    const answer6 = ['turn'];
+    const answer7 = ['final', 'last', 'third', 'next'];
+    const answer8 = ['exceeding'];
+    const answer9 = ['remove', 'clean'];
+    const answer10 = ['sand'];
+    const answer11 = ['latter'];
+    const answer12 = ['following', 'after'];
+
+    let score = 0;
+
+    $('#check-answer').click(checkAnswers) 
+
+    function checkAnswers() {
+        let icon;
+        $(':text').each((idx, item) => {
+            switch(idx) {
+                case 0:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer1) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 1:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer2) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 2:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer3) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 3:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer4) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 4:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer5) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 5:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer6) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 6:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer7) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 7:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer8) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 8:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer9) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 9:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer10) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 10:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer11) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+                case 11:
+                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer12) != -1) {
+                        icon = '<i class="fas fa-check text-success"></i>';
+                         score++;
+                    } else {
+                        icon = '<i class="fas fa-times text-danger"></i>';
+                    }
+                    break;
+            }
+
+            $(item).parent().find('.ans-con').remove();
+            $(item).parent().append(`
+                <div class="ans-con pr-2">
+                    ${icon}
+                </div>
+            `)
+        })
+
+        if(score == 10){
+            alert("you're awesome");
+        }else{
+            alert('Your score is ' + score);
+        }
+    }
+</script>
+@stop
