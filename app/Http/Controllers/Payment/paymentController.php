479e5279dfda8a4f152cb53c0c2e5d5a9f67ed9c
Thanakit.P <78790513+apichart52150@users.noreply.github.com>
2021-08-23 10:09:16 +0700

    Update paymentController.php

--- app/Http/Controllers/Payment/paymentController.php
+++ app/Http/Controllers/Payment/paymentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Payment;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use DB;
 
 class PaymentController extends Controller
