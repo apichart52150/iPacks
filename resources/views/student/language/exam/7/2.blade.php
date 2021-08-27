ec6b8c585dd447300b0db465d99ff78e6e68ef90
Apichart Partoommart <apichart.p@newcambridge.net>
2021-08-27 19:42:55 +0700

    submit

--- resources/views/student/language/exam/7/2.blade.php
+++ resources/views/student/language/exam/7/2.blade.php
@@ -9,11 +9,6 @@
         margin: 10px 5px;
     }
 
-    ul.questions li {
-        margin: 20px 0;
-        line-height: 2rem;
-    }
-
     .input-con {
         display: inline-block;
         position: relative;
@@ -25,22 +20,28 @@
         right: 5px;
         transform: translateY(-50%);
     }
+
+    .line-hight{
+        line-height: 3;
+    }
 </style>
 @php
-    $start = "The line-graphs give details of the percentage of adolescents who owned cd players, laptop computers and mobile phones during the period 1980 to 2010. Overall, it can be seen that the popularity of cd players";
-    $q1 = "significantly, while ownership of mobile phones";
-    $q2 = "dramatically over the period.";
-    $q3 = "At the";
-    $q4 = "of the period cd players were the";
-    $q5 = "popular of all the three gadgets, with more than 80% ownership. Roughly one half of all teenagers had a laptop computer while only fewer";
-    $q6 = "20% had a mobile phone. Over the next decade, mobile phones and laptops had a steady";
-    $q7 = ", while the popularity of cd players experienced a";
-    $q8 = "This trend continued over the next ten years. By the year 2000, only around 40% of young people owned a cd player, while laptop computers reached a peak to stand";
-    $q9 = "80%. Mobile phones also saw a dramatic";
-    $q10 = "to around 70%. By the end of the period, cd players had reached their";
-    $q11 = "popularity, around 30%. Laptop computers also experienced a";
-    $q12 = "during this decade to finish at around 60%. Mobile phones,";
-    $end = ", continued to rise, with almost 100% ownership by 2010.";
+    $startQuestion = "The line-graphs give details of the percentage of adolescents who owned cd players, laptop computers and mobile phones during the period 1980 to 2010. Overall, it can be seen that the popularity of cd players";
+    $question = [
+        'q1' => "significantly, while ownership of mobile phones",
+        'q2' => "dramatically over the period.",
+        'q3' => "At the",
+        'q4' => "of the period cd players were the",
+        'q5' => "popular of all the three gadgets, with more than 80% ownership. Roughly one half of all teenagers had a laptop computer while only fewer",
+        'q6' => "20% had a mobile phone. Over the next decade, mobile phones and laptops had a steady",
+        'q7' => ", while the popularity of cd players experienced a",
+        'q8' => "This trend continued over the next ten years. By the year 2000, only around 40% of young people owned a cd player, while laptop computers reached a peak to stand",
+        'q9' => "80%. Mobile phones also saw a dramatic",
+        'q10' => "to around 70%. By the end of the period, cd players had reached their",
+        'q11' => "popularity, around 30%. Laptop computers also experienced a",
+        'q12' => "during this decade to finish at around 60%. Mobile phones,",
+    ];
+    $endQuestion = ", continued to rise, with almost 100% ownership by 2010.";
 @endphp
 <div class="row">
     <div class="col-md-12">
@@ -54,68 +55,17 @@
                 </div>
                 <div class="col-md-6">
                     <div class="border border-dark p-2">
-                        {{$start}}
-                        {{$q1}}
-                        <span class="font-weight-bold">1.</span> 
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q2}}
-                        <span class="font-weight-bold">2.</span> 
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q3}}
-                        <span class="font-weight-bold">3.</span> 
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q4}}
-                        <span class="font-weight-bold">4.</span> 
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q5}}
-                        <span class="font-weight-bold">5.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q6}}
-                        <span class="font-weight-bold">6.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q7}}
-                        <span class="font-weight-bold">7.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q8}}
-                        <span class="font-weight-bold">8.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q9}}
-                        <span class="font-weight-bold">9.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q10}}
-                        <span class="font-weight-bold">10.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q11}}
-                        <span class="font-weight-bold">11.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$q12}}
-                        <span class="font-weight-bold">12.</span>
-                        <div class="input-con">
-                            <input type="text" class="form-control">
-                        </div>
-                        {{$end}}
+                        {{$startQuestion}}
+                        @for ($i = 1; $i <= count($question); $i++)
+                            <div class="line-hight d-inline w-auto mb-2 ">
+                                {{ $question['q'.$i] }} <!-- question -->
+                            </div>
+                            <span class="font-weight-bold">{{ $i }}</span> <!-- number question-->
+                            <div class="input-con">
+                                <input type="text" class="form-control">
+                            </div>
+                        @endfor
+                        {{$endQuestion}}
                     </div>
                 </div>
             </div>
@@ -131,18 +81,19 @@
 @section('js')
 <script>
 
-    const answer1 = ['decreased', 'fell', 'dropped', 'reduced'];
-    const answer2 = ['increased', 'rose', 'grew'];
-    const answer3 = ['beginning', 'start'];
-    const answer4 = ['most'];
-    const answer5 = ['than'];
-    const answer6 = ['rise','increase'];
-    const answer7 = ['fall','decrease','reduction','drop'];
-    const answer8 = ['at'];
-    const answer9 = ['rise', 'increase'];
-    const answer10 = ['lowest'];
-    const answer11 = ['drop', 'fall', 'decrease'];
-    const answer12 = ['however','though'];
+    const answers = []
+    answers[0] = ['decreased', 'fell', 'dropped', 'reduced'];
+    answers[1] = ['increased', 'rose', 'grew'];
+    answers[2] = ['beginning', 'start'];
+    answers[3] = ['most'];
+    answers[4] = ['than'];
+    answers[5] = ['rise','increase'];
+    answers[6] = ['fall','decrease','reduction','drop'];
+    answers[7] = ['at'];
+    answers[8] = ['rise', 'increase'];
+    answers[9] = ['lowest'];
+    answers[10] = ['drop', 'fall', 'decrease'];
+    answers[11] = ['however','though'];
 
     let score = 0;
 
@@ -151,103 +102,12 @@
     function checkAnswers() {
         let icon;
         $(':text').each((idx, item) => {
-            switch(idx) {
-                case 0:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer1) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 1:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer2) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 2:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer3) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 3:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer4) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 4:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer5) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 5:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer6) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 6:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer7) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 7:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer8) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 8:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer9) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 9:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer10) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 10:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer11) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
-                case 11:
-                    if(jQuery.inArray($(item).val().toLowerCase().replace(/ /g,''),  answer12) != -1) {
-                        icon = '<i class="fas fa-check text-success"></i>';
-                         score++;
-                    } else {
-                        icon = '<i class="fas fa-times text-danger"></i>';
-                    }
-                    break;
+
+            if(jQuery.inArray($(item).val().toLowerCase(),  answers[idx]) != -1) {
+                icon = '<i class="fas fa-check text-success"></i>';
+                score++;
+            } else {
+                icon = '<i class="fas fa-times text-danger"></i>';
             }
 
             $(item).parent().find('.ans-con').remove();
@@ -258,11 +118,8 @@
             `)
         })
 
-        if(score == 12){
-            alert("you're awesome");
-        }else{
-            alert('Your score is ' + score);
-        }
+        alert('Your score is ' + score);
+
     }
 </script>
 @stop
