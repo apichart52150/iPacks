5d9f59f28e4a0740467a92328f96847a7e4519ae
Thanakit.P <78790513+apichart52150@users.noreply.github.com>
2021-09-03 13:08:06 +0700

    Update 3.blade.php

--- resources/views/student/isac/listening/exam/18/3.blade.php
+++ resources/views/student/isac/listening/exam/18/3.blade.php
@@ -15,6 +15,22 @@
         background-color: #e74856;
         border-color: #dee2e6 #dee2e6 #fff;
     }
+
+    .form-control3 {
+        border: none;
+        border-bottom: 1px solid #ccc;
+        display: inline-block;
+        height: auto;
+        width: 140px;
+    }
+
+    .line-break {
+        width: 100%;
+        height: 3px;
+        margin: 10px 0 20px;
+        background: #e74856;
+    }
+
 </style>
 @stop
 @php
@@ -34,30 +50,31 @@
     ];
 
     $question2 = [
-        'q1' => "1. When does your driving licence expire?",
-        'q2' => "2. When does the plane take off?",
-        'q3' => "3. When do the shops open?",
-        'q4' => "4. When do you finish school?",
-        'q5' => "5. When does the library close?",
-        'q6' => "6. When does the programme start?",
-        'q7' => "7. When does daylight saving begin?",
-        'q8' => "8. When does the Christmas sale start?",
-        'q9' => "9. When do you go on holiday?",
-        'q10' => "10. When do you start your new job?",
+        'q1' => "1. There are a lot of people at the party, aren't there?",
+        'q2' => "2. There are a lot of eggs in the basket, aren't there?",
+        'q3' => "3. There's a lot of coffee in the jar, isn't there?",
+        'q4' => "4. There are a lot of books to read, aren't there?",
+        'q5' => "5. There's a lot of wood on the fire, isn't there?",
+        'q6' => "6. There's a lot of paper in the printer, isn't there?",
+        'q7' => "7. There are a lot of animals in the zoo, aren't there?",
+        'q8' => "8. There's a lot of petrol in the tank, isn't there?",
     ];
 
     $question3 = [
-        'q1' => "1. Can you come to a party with me next Saturday?",
-        'q2' => "2. Can you work late this evening?",
-        'q3' => "3. Can you help me with my homework later on?",
-        'q4' => "4. Can you cook dinner tomorrow? ",
-        'q5' => "5. Can you go on holiday with me next week?",
-        'q6' => "6. Can you babysit for me on Saturday night",
-        'q7' => "7. Can you wash the car for me this afternoon?",
-        'q8' => "8. Can you feed my cat when I'm on holida",
-        'q9' => "9. Can you marry me next Sunday?",
-        'q10' => "10. Can you pick up the kids from school today?",
-    ];
+        'q1' => "fish",
+        'q2' => "cheese",
+        'q3' => "carrots",
+        'q4' => "potatoes",
+        'q5' => "lemonade",
+        'q6' => "bread",
+        'q7' => "coffee",
+        'q8' => "eggs",
+        'q9' => "curry",
+        'q10' => "nuts",
+        'q11' => "sandwiches",
+        'q12' => "lollies",
+    ];   
+
 @endphp
 
 <div class="row">
@@ -123,22 +140,18 @@
                 <div class="tab-pane fade" id="exercise2">
                     <table class="table table-sm table-bordered nowrap text-dark mt-3">
                         <thead>
-                            Listen to the example:
+                            <p><u>Listen to the example:</u></p>
+                            
+                            <strong>e.g. 1 </strong>There's a lot of money left, isn't there? 
+                            <p><strong>Answer: </strong>No, there isn't much money left at all.</p>
+
+                            <p><u>Now you do it: </u></p>
                             <tr>
                                 <th class="font-weight-bold">QUESTION</th>
                                 <th class="font-weight-bold">ANSWER</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td><strong>e.g.</strong>When does the train leave?</td>
-                                <td>
-                                    <div class="position-relative">
-                                        <input class="form-control" value="The train leaves in ten minutes' time." disabled>
-                                    </div>
-                                </td>
-                            </tr>
-        
                             @for ($i = 1; $i <= count($question2); $i++)
                                 <tr>
                                     <td>{{ $question2['q'.$i] }}</td>
@@ -155,36 +168,51 @@
                 </div>
 
                 <div class="tab-pane fade" id="exercise3">
-                    <table class="table table-sm table-bordered nowrap text-dark mt-3">
-                        <thead>
-                            Listen to the example:
-                            <tr>
-                                <th class="font-weight-bold">QUESTION</th>
-                                <th class="font-weight-bold">ANSWER</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>e.g.</strong>Can you come to the cinema with me tonight? </td>
-                                <td>
-                                    <div class="position-relative">
-                                        <input class="form-control" value="I'm afraid I can't. I'm washing my hair tonight." disabled>
-                                    </div>
-                                </td>
-                            </tr>
-        
-                            @for ($i = 1; $i <= count($question3); $i++)
-                                <tr>
-                                    <td>{{ $question3['q'.$i] }}</td>
-                                    <td>
-                                        <div class="position-relative">
-                                            <input type="text3" class="form-control">
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endfor
-                        </tbody>
-                    </table>
+                    <p><strong>few</strong> Listen to the examples:</p>
+                    <dl class="row">
+                        <dt class="col-sm-1"><strong>e.g. 1</strong></dt>
+                        <dd class="col-sm-11"><strong>A:</strong> Do you like the <strong>food?</strong></dd>
+                        <dt class="col-sm-1"></dt>
+                        <dd class="col-sm-11"><strong>B:</strong> Yes, <strong>it's</strong> very nice. </dd>
+                        <dt class="col-sm-1"></dt>
+                        <dd class="col-sm-11"><strong>A:</strong> Would you like some more?</dd>
+                        <dt class="col-sm-1"></dt>
+                        <dd class="col-sm-11"><strong>B:</strong> Yes please, but only <strong>a little</strong>. </dd>
+                    </dl>
+                    <dl class="row">
+                        <dt class="col-sm-1"><strong>e.g. 2</strong></dt>
+                        <dd class="col-sm-11"><strong>A:</strong> Do you like the <strong>apples?</strong></dd>
+                        <dt class="col-sm-1"></dt>
+                        <dd class="col-sm-11"><strong>B:</strong> Yes, <strong>they're</strong> very nice. </dd>
+                        <dt class="col-sm-1"></dt>
+                        <dd class="col-sm-11"><strong>A:</strong> Would you like some more?</dd>
+                        <dt class="col-sm-1"></dt>
+                        <dd class="col-sm-11"><strong>B:</strong> Yes please, but only <strong> a few</strong>. </dd>
+                    </dl>
+
+                    <div class="line-break"></div>
+
+                    <p>Now you do it:</p> 
+
+                    @for ($i = 1; $i <= count($question3); $i++)
+                        <dl class="row">
+                            <dt class="col-sm-1"><strong>{{ $i }}</strong></dt>
+                            <dd class="col-sm-11"><strong>A:</strong> Do you like the  
+                                <strong>{{ $question3['q'.$i] }}</strong>
+                            </dd>
+                            <dt class="col-sm-1"></dt>
+                            <dd class="col-sm-11"><strong>B:</strong> Yes,  
+                                <input type="text3" class="form-control3">
+                                very nice. 
+                            </dd>
+                            <dt class="col-sm-1"></dt>
+                            <dd class="col-sm-11"><strong>A:</strong> Would you like some more?</dd>
+                            <dt class="col-sm-1"></dt>
+                            <dd class="col-sm-11"><strong>B:</strong> Yes please, but only  
+                                <input type="text3" class="form-control3">
+                            </dd>
+                        </dl>
+                    @endfor
                     <center> <button id="check-answer3" class="btn btn-info">Check Answersss</button></center>
                 </div>
             </div>
@@ -226,16 +254,42 @@
         ];
 
         const selectAnswer3 = [
-            "I'm afraid I can't. I'm visiting my aunt next Saturday", 
-            "I'm afraid I can't. I'm going to the theatre this evening.", 
-            "I'm afraid I can't. I'm going out later on.", 
-            "I'm afraid I can't. I'm eating out tomorrow.", 
-            "I'm afraid I can't. I'm attending a course next week.", 
-            "I'm afraid I can't. I'm meeting some friends on Saturday night.", 
-            "I'm afraid I can't. I'm playing footie this afternoon.", 
-            "I'm afraid I can't. I'm leaving town when you're on holiday", 
-            "I'm afraid I can't. I'm marrying someone else next Sunday", 
-            "I'm afraid I can't. I'm flying to Sydney today.",
+            "fish", 
+            "it's", 
+            "a little", 
+            "cheese", 
+            "it's", 
+            "a little", 
+            "carrots", 
+            "they're ", 
+            "a few", 
+            "potatoes",
+            "they're",
+            "a few",
+            "lemonade",
+            "it's",
+            "a little",
+            "bread",
+            "it's",
+            "a little",
+            "coffee",
+            "it's",
+            "a little",
+            "eggs",
+            "they're",
+            "a few",
+            "curry",
+            "it's",
+            "a little",
+            "nuts",
+            "they're",
+            "a few",
+            "sandwiches",
+            "they're",
+            "a few",
+            "lollies",
+            "they're",
+            "a few",
         ];
 
         $('#check-answer1').on('click', () => {
