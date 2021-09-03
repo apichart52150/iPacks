1d4e113b1bef30b471a6c9c3bfe4f26c495a4bac
Thanakit.P <78790513+apichart52150@users.noreply.github.com>
2021-09-03 14:12:28 +0700

    submit

--- resources/views/student/isac/listening/exam/2/14.blade.php
+++ resources/views/student/isac/listening/exam/2/14.blade.php
@@ -303,12 +303,13 @@ $caller_2->e12->aw = "and two people are believed to have been drowned by the wo
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <div class="modal-header bg-primary py-2">
-                <h4 class="modal-title text-white mx-auto">Activity 1 - Intro</h4>
+                <h4 class="modal-title text-white mx-auto">Listening - 
+ {{$pageTitle['sub_menu_name'] }}</h4>
             </div>
             <div class="modal-body text-center">
                 <button id="sound-intro" class="btn btn-bordered-primary">Play Sound</button>
                 <audio data-sound="sound-intro">
-                    <source src="{{ asset('public/isac_listening/'.$pageTitle['sub_menu_id'] .'/' .$pageTitle['name_audio']) }}" type="audio/mp3">
+                    <source src="{{ asset('public/isac_listening/'.$pageTitle['sub_menu_type'] .'/' .$pageTitle['name_audio']) }}" type="audio/mp3">
                 </audio>
             </div>
         </div>
