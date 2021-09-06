6e80e914d9e589fea3636cc5f0b031922e8387a6
Apichart Partoommart <apichart.p@newcambridge.net>
2021-09-06 12:17:31 +0700

    Update login.blade.php

--- resources/views/session/login.blade.php
+++ resources/views/session/login.blade.php
@@ -11,9 +11,11 @@
 						<div class="text-center mb-3">
 	                        <h3 class="font-weight-bold text-primary"><i class="fas fa-info"></i> PACKS <span class="d-none d-sm-inline">|</span> <span class="text-dark">LOGIN</span> </h3>
 	                    </div>
-						<p class="text-muted mb-4 mt-3">Enter your username and password to access i.PACK</p>
+						<b class="text-muted mb-4 mt-3">Please access the services via <img alt="" src="https://www.newcambridgethailand.com/mocktest/public/assets/images/1024px-Google_Chrome_icon.png" height="30px"> or <img alt="" src="https://www.newcambridgethailand.com/mocktest/public/assets/images/Firefox_Logo.png" height="30px"></b>
 					</div>
+
 					@if(session()->has('status'))
+					
 					<div class="alert alert-danger alert-dismissible bg-danger text-white border-0 fade show my-2"
 						role="alert">
 						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -45,11 +47,19 @@
 						</div>
 
 					</form>
+
+					<br>
+
+					<b class="text-success">
+						<i class=" mdi mdi-monitor-screenshot"></i> Computers, laptops, or iPads are highly recommended.
+					</b>
+					<b class="text-danger">
+						<i class=" mdi mdi-cellphone-erase"></i>Smartphones should be avoided.
+					</b>
 				</div>
 			</div>
 			<div class="row mt-3">
 				<div class="col-12 text-center">
-					<p> <a href="pages-recoverpw.html" class="text-muted ml-1">Forgot your password?</a></p>
 					<p class="text-muted">Don't have an account? <a href="{{ route('user_register') }}" class="text-white font-weight-medium ml-1">Register</a></p>
 				</div> <!-- end col -->
 			</div>
