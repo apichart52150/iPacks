64715a7c9a6a76237926f012657c70203e34fee7
Apichart Partoommart <apichart.p@newcambridge.net>
2021-09-14 10:06:21 +0700

    submit

--- public/js/rec.js
+++ public/js/rec.js
@@ -1,105 +1,192 @@
-URL = window.URL || window.webkitURL
+URL = window.URL || window.webkitURL;
 
-let gumStream, recorder, input, encodingType
-let encodeAfterRecord = true
+let gumStream, recorder, input, encodingType;
+let encodeAfterRecord = true;
 
-const AudioContext = window.AudioContext || window.webkitAudioContext
-let audioContext
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let audioContext;
 
-const fileType = 'mp3'
+let fileType = "mp3";
+const recordBtn = document.getElementById('recordBtn');
+const loadBtn = document.getElementById('load');
+const resetBtn = document.getElementById('reset');
+const message = document.getElementById('message');
+const spinnerAudio = document.getElementById('spinner-audio');
 
-const recordBtn = document.getElementById('play')
-const finishBtn = document.getElementById('finish')
-const resetBtn = document.getElementById('reset')
-const loadingBtn = document.getElementById('loading')
-const audioContainer = document.getElementById('audio-container')
-const encodeProcessEl = document.getElementById('encode-process')
-
-let fileName
-
-let totalTime, timer
-
-recordBtn.addEventListener('click', startRecording)
-resetBtn.addEventListener('click', () => location.reload())
-recordBtn.addEventListener('click', startRecording)
+recordBtn.addEventListener("click", startRecording);
 
 function startRecording() {
-  let constraints = { audio: true, video: false }
+	console.log("startRecording() called");
 
-  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    audioContext = new AudioContext()
+	let constraints = { audio: true, video: false };
 
-    gumStream = stream
+	navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
 
-    input = audioContext.createMediaStreamSource(stream)
+		audioContext = new AudioContext();
 
-    recorder = new WebAudioRecorder(input, {
-      workerDir: '/ipack/public/js/',
-      encoding: fileType,
-      numChannels: 2,
-      onEncoderLoading: function (recorder, encode) {
-        recordBtn.classList.add('d-none')
-        loadingBtn.classList.remove('d-none')
-      },
-      onEncodingProgress: function (recorder, progress) {
-        if (progress === 1) {
-          encodeProcessEl.remove()
-        }
-      },
-      onTimeout: function (recorder) {
-        recorder.finishRecording()
-        stopTime()
-        loadingBtn.classList.add('d-none')
-        encodeProcessEl.classList.remove('d-none')
-      },
-    })
+		gumStream = stream;
 
-    recorder.setOptions({
-      timeLimit: totalTime,
-      encodeAfterRecord: encodeAfterRecord,
-      mp3: {
-        mimeType: 'audio/mpeg',
-        bitRate: 160,
-      },
-    })
+		input = audioContext.createMediaStreamSource(stream);
+        
+		recorder = new WebAudioRecorder(input, {
+			workerDir: "/isac_speaking/public/js/",
+			encoding: fileType,
+			numChannels: 2,
+			onEncoderLoading: function(recorder, encoding) {
+				recordBtn.classList.add('d-none');
+				$('#icon').removeClass('d-none');
+			},
+		});
 
-    recorder.onComplete = function (recorder, blob) {
-      createDownloadLink(blob, recorder.encoding)
-    }
+		recorder.onComplete = function(recorder, blob) { 
+			createDownloadLink(blob,recorder.encoding);
+		}
 
-    recorder.onError = function (recorder, msg) {
-      console.log(msg)
-    }
+		recorder.setOptions({
+		  timeLimit: 120,
+		  encodeAfterRecord:encodeAfterRecord,
+	      ogg: {quality: 0.5},
+	      mp3: {bitRate: 160}
+	    });
 
-    recorder.startRecording()
+		recorder.startRecording();
+		startTime();
 
-    if (recorder.isRecording()) {
-      startTime()
-    }
-  })
+	}).catch(err => {
+		$('#modalError').modal('show');
+        recordBtn.disabled = false;
+	});
+}
+
+function stopRecording() {
+	console.log('stopRecording() called');
+
+	gumStream.getAudioTracks()[0].stop();
+
+    stopTime();
+
+    $('#icon').addClass('d-none');
+    $("#counter").css("animation-play-state", "paused");
+    $("#counter").html('00:00');
+
+    recordingslist.classList.remove('d-none');
+
+	recorder.finishRecording();
+}
+
+function createDownloadLink(blob,encoding) {
+	
+	let url = URL.createObjectURL(blob);
+	let au = document.createElement('audio');
+	let div = document.createElement('div');
+
+	au.src = url;
+    au.controlsList = "nodownload";
+    au.controls = "controls";
+
+	au.classList.add('m-auto', 'w-100');
+
+    div.classList.add('card', 'card-body', 'p-0', 'p-1');
+
+    div.appendChild(au);
+
+    recordingslist.appendChild(div);
+
+    spinnerAudio.classList.add('d-none');
+    loadBtn.classList.remove('d-none');
+    resetBtn.classList.remove('d-none');
+
+	let queryString = location.pathname;
+    queryString = queryString.split('/');
+
+    let fd = new FormData();
+    fd.append('audio_data', blob, queryString[3]);
+
+    loadBtn.addEventListener('click', () => {
+        $.ajax({
+            type: "POST",
+            url: "/isac_speaking/saveSound",
+            data: fd,
+            processData: false,
+            contentType: false,
+            xhr: function () {
+                var xhr = new window.XMLHttpRequest();
+                //Download progress
+
+                xhr.upload.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = Math.floor(evt.loaded/evt.total * 100);
+                        $('.progress-bar').css({
+                            width: percentComplete + '%'
+                        });
+        
+                        $('.progress-bar').attr('aria-valuenow', percentComplete)
+                        $('.progress-bar').html(percentComplete + '%');
+                    }
+                }, false);
+
+                xhr.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = Math.floor(evt.loaded/evt.total * 100);
+                        $('.progress-bar').css({
+                            width: percentComplete + '%'
+                        });
+        
+                        $('.progress-bar').attr('aria-valuenow', percentComplete)
+                        $('.progress-bar').html(percentComplete + '%');
+                    }
+                }, false);
+                return xhr;
+            },
+            beforeSend: function(evt) {
+                $('#beforesend').removeClass('d-none');
+                message.classList.remove('d-none');
+                loadBtn.disabled = true;
+            },
+            complete: function(data){
+                let msg = JSON.parse(data.responseText);
+                console.log(msg);
+                if(msg.success == "Upload success") {
+                    setTimeout(() => {
+                        let url = `${location.origin}/${queryString[1]}/submit/${queryString[3]}`;
+                        location.href = url;
+                    }, 500)
+                }
+            }
+        })
+    });
 }
 
 // Count Time
+const time = 1;
+let totalTime = time * 3;
+let timer;
+
 function setTime() {
-  totalTime--
+    totalTime--;
 
-  let minutes = Math.floor(totalTime / 60)
-  let seconds = totalTime % 60
+    let minutes = Math.floor(totalTime / 60);
+    let seconds = totalTime % 60;
 
-  let min = minutes < 10 ? '0' + minutes : minutes
-  let sec = seconds < 10 ? '0' + seconds : seconds
+    $('#minutes').html('0' + minutes);
+    $('#seconds').html(seconds < 10 ? '0' + seconds : seconds);
 
-  if(totalTime <= 10) {
-    $('#timer').addClass('text-danger').removeClass('text-primary');
-  }
+    if (totalTime <= 10) {
+        $('#showCount').removeClass('bg-info');
+        $('#showCount').addClass('breathing-color');
+    }
 
-  $('#timer').text(min + ':' + sec)
+    if (totalTime == 0) {
+        stopRecording();
+        stopTime();
+        $("#showCount").css("animation-play-state", "paused");
+    } 
 }
 
 function startTime() {
-  timer = setInterval(setTime, 1000)
+    timer = setInterval(setTime, 1000);
 }
 
 function stopTime() {
-  clearInterval(timer)
+    clearInterval(timer);
 }
