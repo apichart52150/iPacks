10db3ddd687d9a787f5762de18b2e30953c77a31
Apichart Partoommart <apichart.p@newcambridge.net>
2021-10-01 11:47:03 +0700

    Update ViewController.php

--- app/Http/Controllers/student/ipack/writingController/ViewController.php
+++ app/Http/Controllers/student/ipack/writingController/ViewController.php
@@ -12,8 +12,8 @@ class ViewController extends Controller
     public function view_commented($sacId) {
 
         $data = DB::table('text_result')
-        ->select('text_result.id','text_result.score','text_result.comment','text_result.test_type','text_result.code_test','text_result.header_test','text_result.level', 'text_result.status','text_result.th_id','text_result.sent_date', 'text_result.text','text_result.th_text','text_result.mode', 'users.first_name as th_name')
-        ->leftjoin('users','users.id', '=', 'text_result.th_id')
+        ->select('text_result.id','text_result.score','text_result.comment','text_result.test_type','text_result.code_test','text_result.header_test','text_result.level', 'text_result.status','text_result.th_id','text_result.sent_date', 'text_result.text','text_result.th_text','text_result.mode', 'staff.staff_username as th_name')
+        ->leftjoin('staff','staff.staff_id', '=', 'text_result.th_id')
         ->where('text_result.id', '=', $sacId)
         ->get()[0];
 
